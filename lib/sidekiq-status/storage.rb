91c7a48e0e847f756ba2ad174a42301c77cebe3a
FrozenProduce <adam@benchmedia.co.uk>
2012-12-05 10:07:53 +0000

    Returned if the job doc is empty

--- lib/sidekiq-status/storage.rb
+++ lib/sidekiq-status/storage.rb
@@ -2,17 +2,23 @@ module Sidekiq::Status::Storage
   RESERVED_FIELDS=%w(status stop update_time).freeze
 
   def for_entity(entity)
-    SidekiqJob.find(@id).set(:entity_id, entity)
+    job = SidekiqJob.find(@id)
+    return if job.nil?
+    job.set(:entity_id, entity)
   end
 
   def update_message!(message)
     message = { :text => message } if message.is_a? String
-    SidekiqJob.find(@id).set(:message, message)
+    job = SidekiqJob.find(@id)
+    return if job.nil?
+    job.set(:message, message)
     update_timestamps!
   end
 
   def update_status!(status)
-    SidekiqJob.find(@id).set(:status, status)
+    job = SidekiqJob.find(@id)
+    return if job.nil?
+    job.set(:status, status)
     update_timestamps!
   end
 
@@ -23,7 +29,9 @@ module Sidekiq::Status::Storage
   private
 
   def update_timestamps!
-    SidekiqJob.find(@id).set(:updated_at, Time.now)
+    job = SidekiqJob.find(@id)
+    return if job.nil?
+    job.set(:updated_at, Time.now)
   end
 
 end
