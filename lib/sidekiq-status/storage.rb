ef2990ab7b00a9277943784455c4b4cfd9fb2765
John Maxwell <jgwmaxwell@gmail.com>
2014-03-13 15:47:28 +0000

    Update storage.rb

--- lib/sidekiq-status/storage.rb
+++ lib/sidekiq-status/storage.rb
@@ -4,21 +4,21 @@ module Sidekiq::Status::Storage
   def for_entity(entity)
     job = SidekiqJob.find(@id)
     return if job.nil?
-    job.set(:entity_id, entity)
+    job.set(entity_id: entity)
   end
 
   def update_message!(message)
     message = { :text => message } if message.is_a? String
     job = SidekiqJob.find(@id)
     return if job.nil?
-    job.set(:message, message)
+    job.set(message: message)
     update_timestamps!
   end
 
   def update_status!(status)
     job = SidekiqJob.find(@id)
     return if job.nil?
-    job.set(:status, status)
+    job.set(status: status)
     update_timestamps!
   end
 
@@ -31,7 +31,7 @@ module Sidekiq::Status::Storage
   def update_timestamps!
     job = SidekiqJob.find(@id)
     return if job.nil?
-    job.set(:updated_at, Time.now)
+    job.set(updated_at: Time.now)
   end
 
 end
